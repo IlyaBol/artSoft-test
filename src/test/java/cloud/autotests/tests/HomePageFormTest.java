eddc7994d40f1ebc0731977b1c4add76e8a9cb6b
Bolgar <bolgar888@gmail.com>
2021-11-12 18:07:14 +0300

    Comit

--- src/test/java/cloud/autotests/tests/HomePageFormTest.java
+++ src/test/java/cloud/autotests/tests/HomePageFormTest.java
@@ -1,16 +1,13 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.helpers.DriverUtils;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.title;
 import static io.qameta.allure.Allure.step;
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class HomePageFormTest extends TestBase {
 
